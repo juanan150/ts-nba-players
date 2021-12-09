9b7882d0757a59c01370fc9167dc7cdd8bf02bfa
Juan <juanan0309@gmail.com>
2021-12-09 07:05:53 -0500

    Add TS Functionality to the basic app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+import List from './components/List'
+import AddToList from './components/AddToList'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
 }
 
-export default App;
+function App() {
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: 'Lebron james',
+      url: 'https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png',
+      age: 35,
+      note: "He's good",
+    },
+    {
+      name: 'Kobe Bryant',
+      url: 'https://upload.wikimedia.org/wikipedia/commons/4/46/Kobe_B_Bryant.jpg',
+      age: 38,
+    },
+  ])
+
+  return (
+    <div className="App">
+      <h1>People invited to my Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/AddToList.tsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react'
+import { IState as Props } from '../App'
+
+interface IProps {
+  people: Props['people']
+  setPeople: React.Dispatch<React.SetStateAction<Props['people']>>
+}
+
+const AddToList: React.FC<IProps> = ({ people, setPeople }) => {
+  const [input, setInput] = useState({
+    name: '',
+    age: '',
+    note: '',
+    img: '',
+  })
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ): void => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleClick = (): void => {
+    if (!input.name || !input.age || !input.note || !input.img) {
+      alert('Please fill out all fields')
+      return
+    }
+    setPeople([
+      ...people,
+      {
+        name: input.name,
+        age: +input.age,
+        note: input.note,
+        url: input.img,
+      },
+    ])
+  }
+
+  return (
+    <div className="AddToList">
+      <input
+        type="text"
+        placeholder="Name"
+        className="AddToList-input"
+        value={input.name}
+        name="name"
+        onChange={(e) => handleChange(e)}
+      />
+      <input
+        type="text"
+        placeholder="Age"
+        className="AddToList-input"
+        value={input.age}
+        name="age"
+        onChange={(e) => handleChange(e)}
+      />
+      <input
+        type="text"
+        placeholder="Image Url"
+        className="AddToList-input"
+        name="img"
+        value={input.img}
+        onChange={(e) => handleChange(e)}
+      />
+      <textarea
+        placeholder="Notes"
+        className="AddToList-input"
+        name="note"
+        value={input.note}
+        onChange={(e) => handleChange(e)}
+      />
+
+      <button className="AddToList-btn" onClick={handleClick}>
+        Add To List
+      </button>
+    </div>
+  )
+}
+
+export default AddToList
